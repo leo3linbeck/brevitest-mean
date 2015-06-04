6c0fa7754e71cf5903678c84876929a6ed62b8e6
Leo Linbeck III <leo3@linbeck.com>
2015-06-03 21:55:38 -0500

    Email account setup

--- meanjs/config/env/production.js
+++ meanjs/config/env/production.js
@@ -28,12 +28,12 @@ module.exports = {
 		callbackURL: '/auth/github/callback'
 	},
 	mailer: {
-		from: process.env.MAILER_FROM || 'MAILER_FROM',
+		from: process.env.MAILER_FROM || 'brevitest.development@gmail.com',
 		options: {
-			service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+			service: process.env.MAILER_SERVICE_PROVIDER || 'gmail.com',
 			auth: {
-				user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+				user: process.env.MAILER_EMAIL_ID || 'brevitest.development',
+				pass: process.env.MAILER_PASSWORD || '2january88'
 			}
 		}
 	}
